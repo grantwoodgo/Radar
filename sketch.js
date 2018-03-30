942b4a7559e31e384a088789333c8ff9b2ea4091
grantwoodgo <37779597+grantwoodgo@users.noreply.github.com>
2018-03-30 18:53:19 -0400

    500x500 Canvas

--- sketch.js
+++ sketch.js
@@ -4,13 +4,16 @@ var s;
 var tSize;
 var radars=[];
 var numRadars=80; //100 //Resolution of sweep
+var hDisp=0;
+var mDisp=0;
+var sDisp=0;
 
 function preload() {
 	myFont=loadFont('Space Crusader.otf')
 }
 
 function setup() {
-	createCanvas(windowWidth,windowHeight);
+	createCanvas(500,500);
 	background(0);
 	fill(255,0,0,255);
   textFont(myFont);
@@ -26,17 +29,17 @@ function setup() {
 
 function draw() {
 	background(0);
-	translate(width/2,height/2);
+	translate(width/2,height/2-60);
 	if (hour()<=9) {h='0'+hour();}
 	else {h='0'+hour()%12;}
 	if (m<=9) {m='0'+minute();}
 	else {m=minute();}
 	if (s<=9) {s='0'+second();}
 	else {s=second();}
-	tSize=windowWidth/5;
+	tSize=width/5;
 	textSize(tSize);
 	fill(0,255,0);
-	if(radars[0].radR>=PI && radars[0].radR<=(4/3)*PI){
+	if(radars[0].radR>=PI && radars[0].radR<=1.05*PI){
 		hDisp=h;
 		mDisp=m;
 		sDisp=s;
@@ -49,14 +52,13 @@ function draw() {
 		mDisp=mDispPrev;
 		sDisp=sDispPrev;
 	}
-	text(hDisp+':'+mDisp+':'+sDisp,0,0);
-
+	//text(hDisp+':'+mDisp+':'+sDisp,0,0);
+	if(radars[0].radR>=PI || radars[0].radR<PI/3){
+			text(hDisp+':'+mDisp+':'+sDisp,0,0);
+	}
 	for (var i = 0; i < numRadars; i++) {
 		radars[i].update();
 		radars[i].show();
 	}
-}
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
 }
