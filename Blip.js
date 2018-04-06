d97a4cdc0836a952a8bc65c3da106ff38a00a288
grantwoodgo <37779597+grantwoodgo@users.noreply.github.com>
2018-04-06 17:30:19 -0400

    Update Blip.js

--- Blip.js
+++ Blip.js
@@ -1,25 +1,48 @@
 function Blip() {
-  this.r=random(150,fracCase*.9*width/2);
-  this.deg=random(2*PI);
-  this.x=this.r*cos(this.deg);
-  this.y=this.r*sin(this.deg);
-  this.i=0;
+	var rMax=side/2*.85;
+	this.rad=random(0,2*PI);
+	this.r=random(rMax);
+	this.x;
+	this.y;
+	this.frame=0;
+
   this.update=function(){
-    if (radR<=3*PI/4){
-      this.radRTemp=radR+2*PI;
+		if (frameCount-30>=this.frame){
+		this.rad=radR
+		this.r=random(rMax*.1,rMax); //random(rMax);
+		this.x=this.r*cos(this.rad);
+		this.y=this.r*sin(this.rad);
+		this.frame=frameCount;
+		console.log(this.frame);
+		}
+	}
+  this.show=function(){
+    fill(255,0,0);
+    noStroke();
+    ellipse(this.x,this.y,side/50,side/50)
+  }
+}
+/* MOVING BLIPS
+	var rMax=side/2*.85;
+  this.x=random(-rMax,rMax);
+  this.y=random(-sqrt(pow(rMax,2)-pow(this.x,2)),sqrt(pow(rMax,2)-pow(this.x,2)));
+  this.vx=random(-maxBlipSpeed,maxBlipSpeed);
+  this.vy=random(-maxBlipSpeed,maxBlipSpeed);
+
+  this.update=function(){
+    this.x+=this.vx;
+    this.y+=this.vy;
+    if (sqrt(pow(this.x,2)+pow(this.y,2))>=rMax){
+      this.x=random(-rMax,rMax);
+      this.y=random(-sqrt(pow(rMax,2)-pow(this.x,2)),sqrt(pow(rMax,2)-pow(this.x,2)));
+      this.vx=random(-maxBlipSpeed,maxBlipSpeed);
+      this.vy=random(-maxBlipSpeed,maxBlipSpeed);
     }
-    if (this.deg<=this.radRTemp-3*PI/4){
-      this.r=random(0,fracCase*.9*width/2);
-      this.deg=radR;
-      this.x=this.r*cos(this.deg);
-      this.y=this.r*sin(this.deg);
-      console.log('Loop 2');
-    }
-    this.radRTemp=radR;
   }
   this.show=function(){
     fill(0,255,0,100);
     noStroke();
-    ellipse(this.x,this.y,10,10)
+    ellipse(this.x,this.y,side/50,side/50)
   }
 }
+*/
